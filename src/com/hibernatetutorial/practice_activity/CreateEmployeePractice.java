06b79e899f90d5ab58b6f1be1278388aefe102d4
Divya Gupta <divygupta0319@gmail.com>
2021-05-16 18:03:56 +0530

    Added hibernate config file, tested JDBC connection to DB

---
+++ src/com/hibernatetutorial/practice_activity/CreateEmployeePractice.java
@@ -0,0 +1,48 @@
+package com.hibernatetutorial.practice_activity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernatetutorial.practice_activity.entity.Employee;
+
+public class CreateEmployeePractice {
+
+	public static void main(String[] args) {
+		// create session factory
+				SessionFactory factory = new Configuration()
+										.configure("hibernate.cfg.xml")
+										.addAnnotatedClass(Employee.class)
+										.buildSessionFactory();
+
+				
+				// create session
+				Session session = factory.getCurrentSession();
+				
+				try {
+					
+					// create a student object
+					System.out.println("Creating the Employee object...");
+					Employee tempEmployee = new Employee("Donald", "Duck", "Rolls");					
+					
+					// start the transaction
+					session.beginTransaction();
+					
+					
+					// save the student object
+					System.out.println("Saving the employee...");
+					session.save(tempEmployee);
+					
+					
+					// commit transaction
+					session.getTransaction().commit();
+					
+					System.out.println("Done!");
+				}
+				finally {
+					factory.close();
+				}
+
+	}
+
+}
